a192e765931269de0f59c6b11d9b4a604560c0a7
Carlos Zabala <soportezabala@gmail.com>
2023-02-07 15:02:58 -0400

    cambios

---
+++ index.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Happy&Movie</title>
+    <link rel="shortcut icon" href="logop.png" />
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;600&family=Nunito:wght@600;700;800&display=swap" rel="stylesheet" />
+
+    <!-- Icon Font Stylesheet -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <!-- Fuentes del cliente -->
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Sniglet:wght@400;800&display=swap" rel="stylesheet" />
+
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+
+    <!-- BoxIcons -->
+    <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+<style>
+    label{
+        margin-top: 15px;
+    }
+</style>
+</head>
+<body style="background-image: url('back.jpg');">
+    <img src="" alt="">
+    <div class="col-xs-12">
+        <center>
+        <div class="col-md-4 " style="margin-top: 30px;">
+            <img src="logop.png" alt="" >
+            <h1 style="font-size: 40px; color:#FFFFFF; ">Sitio Web <br><b>en Construccion</b></h1><br>
+            <div>
+                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#myModal">
+                    REGISTRAR
+                </button>
+                <h4 style="color: #ffffff">Registrate con nosotros y participa en nuestro sorteo de inauguración</h4>
+            </div>
+
+        </div><br><br><br>
+        <div class="row" style="color: #FFFFFF;">
+        <div class="col-md-4">
+        <a href="https://www.instagram.com/happyandmovietravel/" target="_blank" style="color: #FFFFFF; text-decoration:none"><h4><i class="fa fa-instagram" style="font-size:30px; color:red"></i>   happyandmovietravel</h4></a>
+        </div>
+        
+        <div class="col-md-4">
+            <a href="https://api.whatsapp.com/send?phone=+584248123607&text=%20Te%20asesoramos%20en
+%20la%20eleccion%20de%20tus%20viajes." target="_blank" style="text-decoration:none"><h4 style="color: white; "><i class="fa fa-whatsapp" style="font-size:30px; color:red"></i>   Whatsapp</h4></a>
+        </div>
+        <div class="col-md-4">
+            <h4><i class="fa fa-phone" style="font-size:30px; color:red"></i>  +58 4248123607</h4>
+        </div>
+        
+        </div>
+        </center>
+    </div>
+
+    <div class="modal fade" id="myModal">
+  <div class="modal-dialog">
+    <div class="modal-content">
+
+      <!-- Modal Header -->
+      <div class="modal-header">
+        <h5 class="modal-title"><i class="fa fa-user" style="font-size:20px;"></i> Registrar Usuario</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+      </div>
+
+      <!-- Modal body -->
+    <div class="modal-body">
+        <div class="row">
+            <div class="col-md-6">
+                <label for="name">Nombre*</label>
+                <input class="form-control" type="text" placeholder="Ingresar Nombre" id="name" required>
+            </div>
+            <div class="col-md-6">
+                <label for="name">Apellido*</label>
+                <input class="form-control" type="text" placeholder="Ingresar Apellido" id="name" required>
+            </div>
+        </div>
+        <div class="col-md-12">
+                <label for="name">Correo electronico*</label>
+                <input class="form-control" type="email" placeholder="Email" id="name" required>
+        </div>
+        <div class="col-md-12">
+                <label for="name">Telefono*</label>
+                <input class="form-control" type="number" placeholder="Número de Telefono" id="name">
+        </div>
+        <div class="row">
+            <div class="col-md-6">
+                <label for="name">Contraseña*</label>
+                <input class="form-control" type="text" placeholder="Ingresar Nombre" id="name">
+            </div>
+            <div class="col-md-6">
+                <label for="name">Confirmar Contraseña*</label>
+                <input class="form-control" type="text" placeholder="Ingresar Apellido" id="name">
+            </div>
+        </div>
+    </div>
+
+      <!-- Modal footer -->
+      <div class="modal-footer">
+        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cancelar</button>
+        <button type="button" class="btn btn-info" data-bs-dismiss="modal">Registrar</button>
+      </div>
+
+    </div>
+  </div>
+</div>
+    
+</body>
+</html>
